0193a990c6453ee0c2c02fd90129c40b6392ce71
Aleksandr Mazurenko <neo.katso@gmail.com>
2017-05-27 16:52:46 +0300

    dsl jobs script

---
+++ ci/mnt.groovy
@@ -0,0 +1,49 @@
+import hudson.model.FreeStyleProject
+import hudson.plugins.git.BranchSpec
+import hudson.plugins.git.extensions.impl.RelativeTargetDirectory
+import hudson.plugins.git.extensions.impl.SparseCheckoutPath
+import hudson.plugins.git.extensions.impl.SparseCheckoutPaths
+import hudson.plugins.git.GitSCM
+import hudson.tasks.Shell
+import hudson.triggers.SCMTrigger
+import javaposse.jobdsl.plugin.ExecuteDslScripts
+import javaposse.jobdsl.plugin.*
+import jenkins.model.Jenkins
+import org.jenkinsci.plugins.multiplescms.MultiSCM
+import hudson.plugins.ws_cleanup.PreBuildCleanup
+import hudson.model.ChoiceParameterDefinition
+import hudson.model.ParametersDefinitionProperty
+import org.jenkinsci.plugins.envinject.EnvInjectBuildWrapper
+import org.jenkinsci.plugins.envinject.EnvInjectJobPropertyInfo
+
+def job = { name ->
+    jenkins = Jenkins.instance
+
+    project = new FreeStyleProject(jenkins, name)
+    pollTrigger = new SCMTrigger("*/5 * * * *")
+    project.with {
+        scm = new MultiSCM({
+            ciScm = new GitSCM("git@github.com:EpamHackathon/DemoSpringBootApp.git")
+            ciScm.branches = [new BranchSpec('$BRANCH_NAME')]
+            [ciScm]
+        }())
+
+        cleanupWrapper = new PreBuildCleanup([], false, "", "")
+
+        buildWrappersList.addAll([cleanupWrapper])
+
+        addProperty(new ParametersDefinitionProperty(
+            new StringParameterDefinition("BRANCH_NAME", "master", "")))
+
+        dslBuilder = new ExecuteDslScripts()
+        dslBuilder.setTargets('ci/pipelineJobs.groovy')
+        
+        buildersList.addAll([dslBuilder])
+        createTransientActions()
+        addTrigger(pollTrigger)
+    }
+    jenkins.add(project, name)
+    pollTrigger.start(project, true)
+}
+
+job("DSL")
